7bb9c502bf0848d6ce1724e63acb75c89fa549ff
ravikant706 <ravikant706@gmail.com>
2023-11-25 21:53:08 +0530

    now we check our site show which day today

--- app.js
+++ app.js
@@ -67,16 +67,48 @@ app.get("/", function(req, res){
     //     res.sendFile(__dirname + "/weekday.html");
     // }
 
-
-    var today = new Date();
-    var currentDate = today.getDay();
-    var day = "";
+// //use to boiler plate
+    // var today = new Date();
+    // var currentDate = today.getDay();
+    // var day = "";
 
         
-    if(currentDate === 6 || currentDate === 0) {
-        day = "Weekend";
-    } else {
-        day = "Weekday";
+    // if(currentDate === 6 || currentDate === 0) {
+    //     day = "Weekend";
+    // } else {
+    //     day = "Weekday";
+    // }
+
+
+    var today = new Date();
+    var currentDay = today.getDay();
+    var day = "";
+
+    switch (currentDay) {
+        case 0:
+            day = "Sunday";
+             break;
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Saturday";
+            break;
+            
+        default:
+            console.log("Error: current day is equal to: " + currentDay);
     }
 
     res.render('list', {kindOfDay: day});
