b73b93b3013522a14d5e4a32ab7cb23fd4dd0b9e
ravikant706 <ravikant706@gmail.com>
2023-11-28 19:15:37 +0530

    add another page

--- app.js
+++ app.js
@@ -7,11 +7,14 @@ const ejs = require("ejs");
 
 const app = express();
 
-var items = ["Buy Food", "Cook Food", "Eat Food"];
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
-app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get("/", function(req, res){
     
@@ -117,29 +120,38 @@ app.get("/", function(req, res){
             // }
 
         // use javescript mathod to show day
-                var today = new Date();
-                var options = {
+                let today = new Date();
+                let options = {
                     weekday: "long",
                     day: "numeric",
                     month: "long",
-                    year: "numeric"
                 };
-                var day = today.toLocaleDateString("en-US", options);
+                let day = today.toLocaleDateString("en-US", options);
 
 
-            res.render('list', {kindOfDay: day, newListItems: items});
+            res.render('list', {listTitle: day, newListItems: items});
             
     });
 
 
                 app.post("/", function(req, res) {
-                    var item = req.body.newItem;
+                    let item = req.body.newItem;
 
                     items.push(item);
 
                     res.redirect("/");
                     
+                });
+
+                app.get("/work", function(req, res){
+                    res.render("list", {listTitle: "work list", newListItems: workItems})
                 })
+
+                app.post("/work", function(req, res){
+                    let item = req.body.newItem;
+                    workItems.push(item);
+                    res.redirect("/work");
+                }) 
 
 
 
